3322ce5dd4b57f7a65f3a158dcb9f3cb0d321e07
Daniel Santamaria <santamaria.daniel316@gmail.com>
2024-04-21 20:47:38 -0700

    add signup function for db create

--- server.py
+++ server.py
@@ -1,9 +1,33 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_socketio import SocketIO, emit
+import sqlite3
+from flask_cors import CORS  # Import CORS from flask_cors
+
 
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = 'secret'
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+conn = sqlite3.connect('messaging_app.db')
+conn.execute("PRAGMA foreign_keys = ON;")
+
+
+@app.route('/api/signup', methods=['POST'])
+def signup():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    print('Received message: ' + username + password)  # Access the 'text' property directly
+
+    # Insert new user into the User table
+    try:
+        conn.execute("INSERT INTO User (username, password) VALUES (?, ?)", (username, password))
+        conn.commit()
+        return jsonify({'message': 'User created successfully'}), 201
+    except Exception as e:
+        return jsonify({'error :(': str(e)}), 500
 
 
 # Handle sent messages from clients
@@ -11,14 +35,6 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 def handle_message(message):
     print('Received message: ' + message['text'])  # Access the 'text' property directly
     emit('message', message, broadcast=True)
-
-# @app.route('/api/endpoint', methods=['POST'])
-# def handle_data():
-#     data = request.json
-#     # Process the JSON data as needed
-#     print("Received data from frontend:", data)
-#     emit('message', data, broadcast=True)
-#     return 'Data received successfully'
 
 # Handle 'login' messages from the client
 @socketio.on('login')
